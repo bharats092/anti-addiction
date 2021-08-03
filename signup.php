4f76cfa930b1cd70b895ccc6c540e02b1a29ef3e
bharats092 <bharats092@gmail.com>
2021-08-04 03:09:37 +0530

    test 1

--- signup.php
+++ signup.php
@@ -7,19 +7,20 @@ $sql = "SELECT email, pwd,role FROM user where email='".$email."'";
 $result = $mysqli -> query($sql);
 $row = $result -> fetch_array(MYSQLI_ASSOC);
 //print_r($row);
-$res=array();
-if(count($row)>0){
-    $res=array("code"=>400,"msg"=>"User with same email already exists");
-}else{
-    $sql = "INSERT INTO `user`(`email`, `pwd`) VALUES ('".$email."','".$pwd."')";
-    $result = $mysqli -> query($sql);
-    $id=$mysqli ->insert_id;
-    if($id){
-        $res=array("code"=>200,"msg"=>"Signed up successfully","data"=>$id);
-    }else{
-        $res=array("code"=>400,"msg"=>"Something went wrong");
-    }
-}
+echo "INSERT INTO `user`(`email`, `pwd`) VALUES ('".$email."','".$pwd."')";
+// $res=array();
+// if(count($row)>0){
+//     $res=array("code"=>400,"msg"=>"User with same email already exists");
+// }else{
+//     $sql = "INSERT INTO `user`(`email`, `pwd`) VALUES ('".$email."','".$pwd."')";
+//     $result = $mysqli -> query($sql);
+//     //$id=$mysqli ->insert_id;
+//     if($id){
+//         $res=array("code"=>200,"msg"=>"Signed up successfully");
+//     }else{
+//         $res=array("code"=>400,"msg"=>"Something went wrong");
+//     }
+// }
 
-echo json_encode($res); 
+// echo json_encode($res); 
 ?>
