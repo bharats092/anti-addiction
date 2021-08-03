e4443fdfdd7caa5d6b7c8326e6ead4f5ac7b7322
bharats092 <bharats092@gmail.com>
2021-08-04 03:23:26 +0530

    fixes

--- signup.php
+++ signup.php
@@ -3,19 +3,19 @@ include('config/config.php');
 
 $email=$data['email'];
 $pwd=$data['pwd'];
-$sql = "SELECT email, pwd,role FROM user where email='".$email."'";
-$result = $mysqli -> query($sql);
-$row = $result -> fetch_array(MYSQLI_ASSOC);
-$res=array();
-if(count($row)>0){
-    $res=array("code"=>400,"msg"=>"User with same email already exists");
-}else{
+// $sql = "SELECT email, pwd,role FROM user where email='".$email."'";
+// $result = $mysqli -> query($sql);
+// $row = $result -> fetch_array(MYSQLI_ASSOC);
+// $res=array();
+// if(count($row)>0){
+//     $res=array("code"=>400,"msg"=>"User with same email already exists");
+// }else{
     $sql1 = "INSERT INTO `user`(`email`, `pwd`) VALUES ('".$email."','".$pwd."')";
     $mysqli -> query($sql1);
     //$id=$mysqli ->insert_id;
     $res=array("code"=>200,"msg"=>"Signed up successfully");
    
-}
+//}
 
 echo json_encode($res); 
 ?>
