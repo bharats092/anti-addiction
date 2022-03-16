d4b93f95c99e5950040cc48bdfffd07afb643fd5
Bharat Singh <bharats092@gmail.com>
2022-03-16 20:45:01 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 //$appUrl='http://localhost/anti-addiction/';
 //$mysqli = new mysqli("localhost","root","","anti_addiction");
 $appUrl='https://antiaddiction.herokuapp.com/';
-$mysqli = new mysqli("remotemysql.com","zWpfcRQdWM","V9Zg5Bs4NQ","zWpfcRQdWM");
+$mysqli = new mysqli("remotemysql.com","FA7RqwfHL7","m5EG8qFYpF","FA7RqwfHL7");
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
